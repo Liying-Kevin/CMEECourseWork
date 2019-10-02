d8bb713e4004985acbb98e6169b93d85daea978a
liyinghuang <liying.huang19@imperial.ac.uk>
2019-10-02 10:28:00 +0100

    Added README file.

---
+++ Week1/Code/boilerplate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Author: Your Name your.login@imperial.ac.uk
+# Script: boilerplate.sh
+# Desc: simple boilerplate for shell scripts
+# Arguments: none
+# Date: Oct 2018
+
+echo -e "Remove    excess      spaces.\n" | tr -s "\b" " "
+
+#exit
