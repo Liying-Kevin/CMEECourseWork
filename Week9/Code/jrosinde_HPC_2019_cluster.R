3b7e5a154592876e49dfe424e74b02adf3c7724a
liyinghuang <liying.huang19@imperial.ac.uk>
2020-01-05 14:02:08 +0800

    change

--- Week9/Code/jrosinde_HPC_2019_cluster.R
+++ Week9/Code/jrosinde_HPC_2019_cluster.R
@@ -5,9 +5,9 @@ graphics.off()
 source("jrosinde_HPC_2019_main.R")
 #iter <- as.numeric(Sys.getenv("PBS_ARRAY_INDEX"))
 #set.seed(iter)
-for (i in 1:100) {
+#for (i in 1:100) {
   
-  iter <- sample(100,1,replace = FALSE)
+ # iter <- sample(100,1,replace = FALSE)
   if (0<iter && iter<=25 ) 
     { size = 500
   } else if(25<iter && iter<=50) 
@@ -21,4 +21,4 @@ for (i in 1:100) {
   name <- paste("my_test_file",iter,sep = "-")
   output_file_name <- paste(name,"rda",sep = ".")
   cluster_run(speciation_rate = 0.002048, size, wall_time=11.5*60, interval_rich=1, interval_oct=size/10 , burn_in_generations=8*size, output_file_name) 
-}
+#}
